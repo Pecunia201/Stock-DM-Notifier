25892b7ca1264bde2716a181472f521d4a638a18
Laurie <laurie201@protonmail.ch>
2023-07-08 00:12:36 +0100

    Add configuration file, RSI calculation script, and update main functionality
    - Modified README.md with additional information
    - Added config.json file for storing configuration settings
    - Modified main.py to incorporate new features
    - Created requirements.txt to document project dependencies
    - Added rsi.py script for calculating Relative Strength Index (RSI)

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
+import datetime
+import asyncio
+import discord
+import rsi
+from discord.ext import commands, tasks
+import json
 
+# Read the JSON file
+with open('Stock-DM-Notifier\config.json') as file:
+    data = json.load(file)
+
+# Access the values
+discord_token = data['discord_token']
+discord_user_id = data['discord_user_id']
+rsi_threshold = data['rsi_threshold']
+
+# Create a new bot instance with intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@tasks.loop(count=1)
+async def send_message():
+    await bot.wait_until_ready()
+
+    while True:
+        now = datetime.datetime.now()
+        rsi_result = rsi.rsi()
+
+        if now.hour == 5 and now.minute == 0 and rsi_result < rsi_threshold:
+            try:
+                # Fetch the user object using the target user ID
+                user = await bot.fetch_user(discord_user_id)
+                # Send a direct message to the user
+                await user.send(f"RSI has dropped below 30: {round(rsi_result, 2)}")
+                print('Message sent successfully!')
+            except discord.HTTPException:
+                print('Failed to send the message.')
+        
+        # Wait for 1 minute before checking the time again
+        await asyncio.sleep(60)
+
+@bot.event
+async def on_ready():
+    print(f'Bot is ready and logged in as {bot.user.name}')
+    send_message.start()
+
+# Run the bot
+bot.run(discord_token)
---
+++ rsi.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import talib
+import json
+
+# Read the JSON file
+with open('Stock-DM-Notifier\config.json') as file:
+    data = json.load(file)
+
+# Access the values
+stock_symbol = data['stock_symbol']
+rsi_period = data['rsi_period']
+
+def rsi():
+    # Define the ticker symbol for the S&P 500
+    ticker = yf.Ticker(stock_symbol)
+
+    # Download historical data for the S&P 500
+    data = ticker.history(period="max", interval="1d", prepost = True)
+
+    # Extract closing prices from the data
+    close_prices = data["Close"].values
+
+    # Calculate RSI using talib
+    rsi = talib.RSI(close_prices, timeperiod=rsi_period)
+
+    # Get the last RSI value and its corresponding date
+    current_rsi = rsi[-1]
+    #current_date = data.index[-1]
+
+    return current_rsi
